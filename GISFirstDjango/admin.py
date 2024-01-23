71a7f8cca4109f498cd0026c4d9e8d633671a185
James Wright <jdjwright@gmail.com>
2024-01-23 08:21:34 +0800

    Initial files for lesson 1

---
+++ GISFirstDjango/admin.py
@@ -0,0 +1,26 @@
+from django.apps import apps
+from django.contrib import admin
+
+
+def register_all_app_models():
+    """
+    Register any unregistered app models. We call this function    after registering any custom     admin classes.
+     """
+    models_to_ignore = [
+        'admin.LogEntry',
+        'contenttypes.ContentType',
+        'sessions.Session',
+        'authtoken.TokenProxy',
+        'authtoken.Token',  # We automatically register the authtoken app models.
+    ]
+    for model in apps.get_models():
+        try:
+            if model._meta.label in models_to_ignore:
+                continue
+            else:
+                class modelAdmin(admin.ModelAdmin):
+                    list_display = [field.name for field in model._meta.fields]
+
+                admin.site.register(model, modelAdmin)
+        except admin.sites.AlreadyRegistered:
+            pass
