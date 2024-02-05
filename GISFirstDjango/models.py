ae19226dba082536408875cda4eb3fb0e0ace798
James Wright <jdjwright@gmail.com>
2024-02-05 15:16:22 +0800

    Added simple filtering

--- GISFirstDjango/models.py
+++ GISFirstDjango/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    age = models.IntegerField(blank=False, null=True)
 
 class TeachingGroup(models.Model):
     students = models.OneToOneField(Person, null=True, on_delete=models.SET_NULL)
